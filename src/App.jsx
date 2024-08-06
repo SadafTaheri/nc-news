1d4c83eaee5e2abea65610457df2d3d8c733f07c
SADAF MTAHERI <sadaf@SADAFs-MacBook-Pro.local>
2024-08-06 10:14:20 +0100

    Routing and complete planning

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import ArticlesList from "./components/AriclesList";
+import Topics from "./components/Topicts";
+import Login from "./components/Login";
+import Header from "./components/Header";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-  return <h1>NC News</h1>;
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/articles" element={<ArticlesList />} />
+        <Route path="/topics" element={<Topics />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
